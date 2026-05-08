983627190562ba9478c77b6e0c59075a197f6922
syngrid <repository@syngrid.com>
2026-05-08 17:20:08 +0530

    Update CORS origin to sk8engineering.netlify.app

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.get('/api/health', (req, res) => {
 // Middleware
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin: 'sk8engineering.netlify.app',
     credentials: true
 }));
 app.use(express.json());
