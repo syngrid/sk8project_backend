955a3288fc1550eb829d2fd8c21325a0b9b94ee7
syngrid <repository@syngrid.com>
2026-05-08 16:28:50 +0530

    Add health check endpoint to server

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/api/health', (req, res) => {
+    res.send('API is running');
+});
+
 // Middleware
 app.use(morgan('dev'));
 app.use(cors({
